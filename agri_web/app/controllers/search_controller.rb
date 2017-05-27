02a63a62a3437bc5f99a1dac6ae3b9a773edbe61
ReiA <evyholic@gmail.com>
2017-05-27 16:54:14 +0900

    recipe

--- agri_web/app/controllers/search_controller.rb
+++ agri_web/app/controllers/search_controller.rb
@@ -3,6 +3,21 @@ class SearchController < ApplicationController
 
   def index
     @vegetable_informations = VegetableInformation.search(search_parmas)
+    search_recipe(@vegetable_informations.first.vegetable.name)
+  end
+
+  def search_recipe(item)
+    cookpad_url = 'https://cookpad.com/search/'
+    url_escape = URI.escape(item)
+    charset = nil
+    html = open(cookpad_url + url_escape) do |f|
+      f.read
+    end
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+
+    recipes = doc.css(".recipe-preview")
+    @titles = recipes.css(".recipe-title")
+    @images = recipes.css("img").map{|i| i.attribute('src').value}
   end
 
   private
